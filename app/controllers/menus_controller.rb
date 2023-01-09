389cd5d1a42a5b7e181e8d0a2e45f427140628a9
IrinaLepekhina <lepekhina-irina@yandex.ru>
2023-01-09 11:12:27 -0600

    acceptance tests

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -5,20 +5,20 @@ class MenusController < ApplicationController
     #   @menu = Menu.find(params[:id])
     # end
   
-    # def new
-    #   @menu = Menu.new
-    # end
+    def new
+      @menu = Menu.new
+    end
   
-    # def create
-    #   @menu = Menu.new(menu_params)
-    #   if @menu.save
-    #     redirect_to root_url, notice: 'Menu has been created'
-    #   else
-    #     render :new
-    #   end
-    # end
+    def create
+      @menu = Menu.new(menu_params)
+      if @menu.save
+        redirect_to root_url, notice: 'Menu has been created'
+      else
+        render :new
+      end
+    end
   
-    # def menu_params
-    #   params.require(:menu).permit(:title, :meal_id, :price) 
-    # end
+    def menu_params
+      params.require(:menu).permit(:title, :date) 
+    end
   end
